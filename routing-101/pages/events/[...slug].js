9cc332bf9b1d2cecd0118f2acc1b45b3caa6aa73
rmnsrs <ramanshrestha@eeposit.com>
2022-02-19 18:15:43 +0545

    Routing Project Complete

--- routing-101/pages/events/[...slug].js
+++ routing-101/pages/events/[...slug].js
@@ -1,7 +1,43 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
 function FilteredEvents() {
+	const router = useRouter();
+	const filteredData = router.query.slug;
+
+	if (!filteredData) {
+		return <p className="center"> Loading ...</p>;
+	}
+
+	const [filteredYear, filteredMonth] = filteredData;
+
+	const numYear = +filteredYear;
+	const numMonth = +filteredMonth;
+
+	console.log(numMonth, numYear, "hahha");
+
+	if (
+		isNaN(numYear) ||
+		isNaN(numMonth) ||
+		numYear > 2030 ||
+		numYear < 2021 ||
+		numMonth < 1 ||
+		numMonth > 12
+	) {
+		return <p> Invalid Filter, Please Enter valid Filter Values </p>;
+	}
+
+	const filteredEvents = getFilteredEvents({
+		year: numYear,
+		month: numMonth,
+	});
+
+	if (!filteredEvents || filteredEvents.length === 0) {
+		return <p> No Events Found For Filtered Event </p>;
+	}
 	return (
 		<div>
-			<p> Hi I am FilteredEvents Page </p>
+			<EventList item={filteredEvents} />
 		</div>
 	);
 }
--- routing-101/pages/events/index.js
+++ routing-101/pages/events/index.js
@@ -1,9 +1,18 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 function AllEventsPage() {
 	const events = getAllEvents();
+	const router = useRouter();
+
+	const findEventsHandler = (year, month) => {
+		const fullPath = `/events/${year}/${month}`;
+		router.push(fullPath);
+	};
 	return (
 		<div>
+			<EventsSearch onSearch={findEventsHandler} />
 			<EventList item={events} />
 		</div>
 	);
