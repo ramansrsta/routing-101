29747f804f72abbd1d14924ee152e552884eef54
rmnsrs <ramanshrestha@eeposit.com>
2022-04-06 21:00:03 +0545

    adding prerendering

--- routing-101/pages/events/[eventId].js
+++ routing-101/pages/events/[eventId].js
@@ -1,14 +1,11 @@
-import { useRouter } from "next/router";
 import { Fragment } from "react";
-import { getEventById } from "../../dummy-data";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 
-function EventsDetail() {
-	const router = useRouter();
-	const eventId = router.query.eventId;
-	const event = getEventById(eventId);
+function EventsDetail(props) {
+	const event = props.selectedEvent;
 
 	if (!event) {
 		return <p> No Event Found </p>;
@@ -28,6 +25,28 @@ function EventsDetail() {
 			</EventContent>
 		</Fragment>
 	);
+}
+
+export async function getStaticProps(context) {
+	const eventId = context.params.eventId;
+	const event = await getEventById(eventId);
+
+	return {
+		props: {
+			selectedEvent: event,
+		},
+		revalidate: 30,
+	};
+}
+
+export async function getStaticPaths() {
+	const events = await getFeaturedEvents();
+
+	const paths = events.map((event) => ({ params: { eventId: event.id } }));
+	return {
+		paths: paths,
+		fallback: "blocking",
+	};
 }
 
 export default EventsDetail;
--- routing-101/pages/index.js
+++ routing-101/pages/index.js
@@ -1,13 +1,22 @@
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 
-function HomePage() {
-	const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
 	return (
 		<div>
-			<EventList item={featuredEvents} />
+			<EventList item={props.events} />
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	const featuredEvents = await getFeaturedEvents();
+	return {
+		props: {
+			events: featuredEvents,
+		},
+		revalidate: 1800,
+	};
 }
 
 export default HomePage;
