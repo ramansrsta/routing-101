fb1822986edd26e5be29d6dceb5a4263989d7dbf
rmnsrs <ramanshrestha@eeposit.com>
2022-04-11 13:45:44 +0545

    Optimization

--- routing-101/pages/events/[eventId].js
+++ routing-101/pages/events/[eventId].js
@@ -3,6 +3,7 @@ import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import Head from "next/head";
 
 function EventsDetail(props) {
 	const event = props.selectedEvent;
@@ -13,6 +14,10 @@ function EventsDetail(props) {
 
 	return (
 		<Fragment>
+			<Head>
+				<title>{event.title} </title>
+				<meta name="description" content={event.description} />
+			</Head>
 			<EventSummary title={event.title} />
 			<EventLogistics
 				date={event.date}
--- routing-101/pages/index.js
+++ routing-101/pages/index.js
@@ -1,9 +1,14 @@
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 
 function HomePage(props) {
 	return (
 		<div>
+			<Head>
+				<title>Next Js Events </title>
+				<meta name="description" content="Finde Events" />
+			</Head>
 			<EventList item={props.events} />
 		</div>
 	);
